39179d5f177b49e7e9322fa889a2d1605e0c9a4e
Stephen Young <stephenjayyoung@gmail.com>
2016-03-26 12:39:43 -0700

    dev env/working on exercises

---
+++ js/MainController.js
@@ -0,0 +1,37 @@
+app.controller('MainController', ['$scope', function($scope) { 
+  $scope.title = 'This string is my own'; 
+  $scope.promo = 'This is the promo string';
+  $scope.products = [ 
+  { 
+    name: 'The Book of Trees', 
+    price: 19, 
+    pubdate: new Date('2014', '03', '08'), 
+    cover: 'img/the-book-of-trees.jpg',
+    likes: 0
+  }, 
+  { 
+    name: 'Program or be Programmed', 
+    price: 8, 
+    pubdate: new Date('2013', '08', '01'), 
+    cover: 'img/program-or-be-programmed.jpg',
+    likes: 0
+  }, 
+      { 
+    name: 'The Book ', 
+    price: 19, 
+    pubdate: new Date('2014', '03', '08'), 
+    cover: 'img/the-book-of-trees.jpg',
+    likes: 0
+  }, 
+  { 
+    name: 'Program', 
+    price: 8, 
+    pubdate: new Date('2013', '08', '01'), 
+    cover: 'img/program-or-be-programmed.jpg',
+    likes: 0
+  }
+],
+  $scope.plusOne = function(index) { 
+  $scope.products[index].likes += 1; 
+};
+}]);
